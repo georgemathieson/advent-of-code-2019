0c5feaa1b6e50c22096130e498353819f2d881da
George Mathieson <george@mathieson.me>
2019-12-04 22:28:56 +0000

    Add solution for day 3

--- 03/03.py
+++ 03/03.py
@@ -13,7 +13,8 @@ deltas = {
 def find_points(wire):
     points = set()
     paths = wire.split(',')
-    x, y = 0, 0
+    x, y, steps = 0, 0, 0
+    numberOfSteps = {}
     for path in paths:
         direction = path[:1]
         length = int(path[1:])
@@ -23,14 +24,16 @@ def find_points(wire):
             x += deltaX
             y += deltaY
             points.add((x, y))
-    return points
+            steps += 1
+            numberOfSteps.setdefault((x, y), steps)
+    return points, numberOfSteps
 
 # Puzzle input.
 with open('input') as puzzleInput:
     wires = [value for value in puzzleInput.read().splitlines()]
 
-pointsWire1 = find_points(wires[0])
-pointsWire2 = find_points(wires[1])
+pointsWire1, numberOfStepsWire1 = find_points(wires[0])
+pointsWire2, numberOfStepsWire2 = find_points(wires[1])
 
 # We only care about where both wire 1 and wire 2 cross paths.
 intersections = pointsWire1 & pointsWire2
@@ -40,3 +43,9 @@ distances = [manhattan_distance(x, y) for x, y in intersections]
 
 # Part 1 answer.
 print(min(distances))
+
+# The combined steps the wires must take to reach an intersection.
+steps = [numberOfStepsWire1[(x, y)] + numberOfStepsWire2[(x, y)] for x, y in intersections]
+
+# Part 2 answer
+print(min(steps))
